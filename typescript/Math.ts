6ec42f5908e010c4bd16fd33ea32a761de341924
Don Williamson <dwilliamson_coder@hotmail.com>
2017-12-15 21:16:29 +0000

    Round inputs to int2

--- typescript/Math.ts
+++ typescript/Math.ts
@@ -1,4 +1,12 @@
 
+// Internal operations of int2 can assume inputs are already integer and avoid rounding
+enum int2Round
+{
+    Do,
+    Dont
+}
+
+
 class int2
 {
     x: number;
@@ -7,78 +15,68 @@ class int2
     static readonly Zero = new int2(0, 0);
     static readonly One = new int2(1, 1);
 
-    constructor(x: number = 0, y: number = x)
+    constructor(x: number = 0, y: number = x, round: int2Round = int2Round.Do)
     {
-        this.x = x;
-        this.y = y;
+        if (round == int2Round.Do)
+        {
+            this.x = Math.round(x);
+            this.y = Math.round(y);
+        }
+        else
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 
     Copy() : int2
     {
-        return new int2(this.x, this.y);
+        return new int2(this.x, this.y, int2Round.Dont);
     }
 
     static Add(a: int2, b: int2) : int2
     {
-        return new int2(
-            a.x + b.x,
-            a.y + b.y);
+        return new int2(a.x + b.x, a.y + b.y, int2Round.Dont);
     }
 
     static Sub(a: int2, b: int2) : int2
     {
-        return new int2(
-            a.x - b.x,
-            a.y - b.y);
+        return new int2(a.x - b.x, a.y - b.y, int2Round.Dont);
     }
 
     static Mul(a: int2, b: int2) : int2
     {
-        return new int2(
-            a.x * b.x,
-            a.y * b.y);
+        return new int2(a.x * b.x, a.y * b.y, int2Round.Dont);
     }
 
     static Min(a: int2, b: int2) : int2
     {
-        return new int2(
-            Math.min(a.x, b.x),
-            Math.min(a.y, b.y));
+        return new int2(Math.min(a.x, b.x), Math.min(a.y, b.y), int2Round.Dont);
     }
 
     static Max(a: int2, b: int2) : int2
     {
-        return new int2(
-            Math.max(a.x, b.x),
-            Math.max(a.y, b.y));
+        return new int2(Math.max(a.x, b.x), Math.max(a.y, b.y), int2Round.Dont);
     }
 
     static Min0(a: int2) : int2
     {
-        return new int2(
-            Math.min(a.x, 0),
-            Math.min(a.y, 0));
+        return new int2(Math.min(a.x, 0), Math.min(a.y, 0), int2Round.Dont);
     }
 
     static Max0(a: int2) : int2
     {
-        return new int2(
-            Math.max(a.x, 0),
-            Math.max(a.y, 0));
+        return new int2(Math.max(a.x, 0), Math.max(a.y, 0), int2Round.Dont);
     }
 
     static Neg(a: int2) : int2
     {
-        return new int2(
-            -a.x,
-            -a.y);
+        return new int2(-a.x, -a.y, int2Round.Dont);
     }
 
     static Abs(a: int2) : int2
     {
-        return new int2(
-            Math.abs(a.x),
-            Math.abs(a.y));
+        return new int2(Math.abs(a.x), Math.abs(a.y), int2Round.Dont);
     }
 
     static Equal(a: int2, b: int2) : boolean
