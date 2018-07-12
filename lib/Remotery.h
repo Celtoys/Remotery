a6ec305c09363a28deb78534b299ae801fc02ae7
Don Williamson <dwilliamson@users.noreply.github.com>
2018-07-12 15:47:58 +0100

    Update copyright dates

--- lib/Remotery.h
+++ lib/Remotery.h
@@ -1,7 +1,7 @@
 
 
 /*
-Copyright 2014 Celtoys Ltd
+Copyright 2014-2018 Celtoys Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
