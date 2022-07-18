f1f11ddc7ed3b2d0fcc76e8ae8c5aa8cd4d7f763
JCash <mwesterdahl76@gmail.com>
2022-07-18 17:10:34 +0200

    Added support for Android 10+

--- .github/scripts/compile.sh
+++ .github/scripts/compile.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-ARCH=$1
+PLATFORM=$1
+ARCH=$2
+BUILDMODE=$3
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ ! -e build ]; then
     mkdir -p build
@@ -15,48 +19,79 @@ function run {
     $args
 }
 
-if [ "${ARCH}" == "x64" ]; then
-    ARCH="-m64 -DRMT_ARCH_64BIT"
+function quit_on_error {
+    echo "$*"
+    exit 1
+}
+
+if [ "${ARCH}" == "x64" ] || [ "${ARCH}" == "arm64" ]; then
+    ARCH_FLAGS="-m64 -DRMT_ARCH_64BIT"
 else
-    ARCH="-m32"
+    ARCH_FLAGS="-m32"
 fi
 
-
 LINKFLAGS="-lm -pthread"
-if [ "$(uname)" == "Darwin" ]; then
+if [ "macos" == "${PLATFORM}" ]; then
     LINKFLAGS=""
 fi
 
+if [ "android" == "${PLATFORM}" ]; then
+    . ${SCRIPT_DIR}/android_env.sh
+else
+    GCC=$(which gcc)
+    GXX=$(which g++)
+    CLANGCC=$(which clang)
+    CLANGXX=$(which clang++)
+    echo "GCC=${GCC}"
+    echo "GXX=${GXX}"
+    echo "CLANGCC=${CLANGCC}"
+    echo "CLANGXX=${CLANGXX}"
+fi
+
+if [ "" == "${GCC}" ]; then
+    quit_on_error "No GCC speficied"
+fi
+if [ "" == "${GXX}" ]; then
+    quit_on_error "No GXX speficied"
+fi
+if [ "" == "${CLANGCC}" ]; then
+    quit_on_error "No CLANGCC speficied"
+fi
+if [ "" == "${CLANGXX}" ]; then
+    quit_on_error "No CLANGXX speficied"
+fi
+
 # C
-run gcc -c ${ARCH} lib/Remotery.c
-run gcc -c ${ARCH} -x c++ lib/Remotery.c
-run gcc -c ${ARCH} -D RMT_USE_OPENGL=1 lib/Remotery.c
-run gcc -c ${ARCH} -D RMT_USE_OPENGL=1 -x c++ lib/Remotery.c
-run clang -c ${ARCH} lib/Remotery.c
-run clang -c ${ARCH} -xc++ lib/Remotery.c
-run clang -c ${ARCH} -DRMT_USE_OPENGL=1 lib/Remotery.c
-run clang -c ${ARCH} -xc++ -DRMT_USE_OPENGL=1 lib/Remotery.c
+
+run ${GCC} -c ${ARCH_FLAGS} lib/Remotery.c
+run ${GCC} -c ${ARCH_FLAGS} -x c++ lib/Remotery.c
+run ${GCC} -c ${ARCH_FLAGS} -D RMT_USE_OPENGL=1 lib/Remotery.c
+run ${GCC} -c ${ARCH_FLAGS} -D RMT_USE_OPENGL=1 -x c++ lib/Remotery.c
+run ${CLANGCC} -c ${ARCH_FLAGS} lib/Remotery.c
+run ${CLANGCC} -c ${ARCH_FLAGS} -xc++ lib/Remotery.c
+run ${CLANGCC} -c ${ARCH_FLAGS} -DRMT_USE_OPENGL=1 lib/Remotery.c
+run ${CLANGCC} -c ${ARCH_FLAGS} -xc++ -DRMT_USE_OPENGL=1 lib/Remotery.c
 
 # C++
 run cp lib/Remotery.c build/Remotery.cpp
-run g++ -c ${ARCH} -Ilib build/Remotery.cpp
-run g++ -c ${ARCH} -Ilib -x c++ build/Remotery.cpp
-run g++ -c ${ARCH} -Ilib -D RMT_USE_OPENGL=1 build/Remotery.cpp
-run g++ -c ${ARCH} -Ilib -D RMT_USE_OPENGL=1 -x c++ build/Remotery.cpp
-run clang++ -c ${ARCH} -Ilib build/Remotery.cpp
-run clang++ -c ${ARCH} -Ilib -xc++ build/Remotery.cpp
-run clang++ -c ${ARCH} -Ilib -DRMT_USE_OPENGL=1 build/Remotery.cpp
-run clang++ -c ${ARCH} -Ilib -xc++ -DRMT_USE_OPENGL=1 build/Remotery.cpp
+run ${GXX} -c ${ARCH_FLAGS} -Ilib build/Remotery.cpp
+run ${GXX} -c ${ARCH_FLAGS} -Ilib -x c++ build/Remotery.cpp
+run ${GXX} -c ${ARCH_FLAGS} -Ilib -D RMT_USE_OPENGL=1 build/Remotery.cpp
+run ${GXX} -c ${ARCH_FLAGS} -Ilib -D RMT_USE_OPENGL=1 -x c++ build/Remotery.cpp
+run ${CLANGXX} -c ${ARCH_FLAGS} -Ilib build/Remotery.cpp
+run ${CLANGXX} -c ${ARCH_FLAGS} -Ilib -xc++ build/Remotery.cpp
+run ${CLANGXX} -c ${ARCH_FLAGS} -Ilib -DRMT_USE_OPENGL=1 build/Remotery.cpp
+run ${CLANGXX} -c ${ARCH_FLAGS} -Ilib -xc++ -DRMT_USE_OPENGL=1 build/Remotery.cpp
 
 # enable disable
-run clang ${ARCH} ${LINKFLAGS} -DRMT_ENABLED=0 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
-run clang ${ARCH} ${LINKFLAGS} -DRMT_ENABLED=1 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -DRMT_ENABLED=0 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -DRMT_ENABLED=1 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
 
 # custom hash function
-run clang ${ARCH} ${LINKFLAGS} -DRMT_USE_INTERNAL_HASH_FUNCTION=0 -Ilib sample/sample.c .github/scripts/hashfunction.c lib/Remotery.c -o build/dummy
-run clang ${ARCH} ${LINKFLAGS} -DRMT_USE_INTERNAL_HASH_FUNCTION=1 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -DRMT_USE_INTERNAL_HASH_FUNCTION=0 -Ilib sample/sample.c .github/scripts/hashfunction.c lib/Remotery.c -o build/dummy
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -DRMT_USE_INTERNAL_HASH_FUNCTION=1 -Ilib sample/sample.c lib/Remotery.c -o build/dummy
 
 # samples
-run clang ${ARCH} ${LINKFLAGS} -Ilib sample/sample.c lib/Remotery.c -o build/sample
-run clang ${ARCH} ${LINKFLAGS} -Ilib sample/dump.c lib/Remotery.c -o build/dump
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -Ilib sample/sample.c lib/Remotery.c -o build/sample
+run ${CLANGCC} ${ARCH_FLAGS} ${LINKFLAGS} -Ilib sample/dump.c lib/Remotery.c -o build/dump
 
