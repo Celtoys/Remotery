ddfd43c84218b00fe01b2ff85595cc13da6cd552
JCash <mwesterdahl76@gmail.com>
2022-07-19 09:17:35 +0200

    review fixes

--- lib/Remotery.c
+++ lib/Remotery.c
@@ -1013,7 +1013,23 @@ error:
     return ret;
 }
 
-// https://chromium.googlesource.com/chromium/src/+/HEAD/third_party/ashmem/ashmem-dev.c
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// https://chromium.googlesource.com/chromium/src/+/ad02487d87120bd66045960ffafe0fc27600af50/third_party/ashmem/ashmem-dev.c#181
 
 // Starting with API level 26, the following functions from
 // libandroid.so should be used to create shared memory regions.
